899ca0b407bc3c44c703d5593b6f83e03607083d
Exevald <kalinin.konst.official@gmail.com>
2022-05-19 18:47:45 +0300

    Добавили генерацию новых шариков со случайной скоростью

--- CanvasGame/src/js/script.js
+++ CanvasGame/src/js/script.js
@@ -4,25 +4,49 @@ var GAME = {
     ifLost: false,
 }
 
+var BOMBS = [];
+
+var countOfBombs = 2;
+var bombSpeed = 20;
+var bombSize = 20;
+
+var InfoWindow = {
+    width: 200,
+    height: GAME.height,
+    x: GAME.width
+}
+
 var PLAYER = {
     x: GAME.width * 0.45,
     y: GAME.height * 0.9,
     height: GAME.height * 0.03,
     width: GAME.width * 0.07,
     score: 0,
-}
-
-var BOMB = {
-    x: 50,
-    y: 50,
-    speedy: 20,
-    size: 25,
+    lives: 3,
+    speedX: 20,
 }
 
 var canvas = document.getElementById("canvas");
 var canvasContext = canvas.getContext("2d");
-canvas.width = GAME.width;
+canvas.width = GAME.width + InfoWindow.width;
 canvas.height = GAME.height;
+
+function InitBombs() {
+    var i = 0;
+    do {
+        var initX = Math.floor(Math.random() * (GAME.width - bombSize));
+        var initSpeed = Math.floor(Math.random() * 20 + 5);
+        BOMBS[i] = {
+            x: initX,
+            y: 0,
+            speedy: initSpeed,
+            size: 20,
+        }
+        console.log(i);
+        i++;
+    }
+    while (i < countOfBombs)
+}
 
 function drawBackground() {
     canvasContext.fillStyle = "red";
@@ -35,34 +59,78 @@ function drawPlayer() {
 }
 
 function drawBomb() {
-    canvasContext.fillStyle = "black";
-    canvasContext.beginPath();
-    canvasContext.arc(BOMB.x, BOMB.y, BOMB.size, 0, Math.PI * 2);
-    canvasContext.fill();
+    for (var i = 0; i < countOfBombs; i++) {
+        canvasContext.fillStyle = "black";
+        canvasContext.beginPath();
+        canvasContext.arc(BOMBS[i].x, BOMBS[i].y, BOMBS[i].size, 0, Math.PI * 2);
+        canvasContext.fill();
+    }
 }
 
-function updateBomb() {
-    var losePositionY = BOMB.y + BOMB.size / 2 >= PLAYER.y;
-    var losePositionX = (BOMB.x - BOMB.size / 2 <= PLAYER.x + PLAYER.width) && (BOMB.x + BOMB.size / 2 >= PLAYER.x);
-    var scoreUpdate = (BOMB.y >= GAME.height - BOMB.size) && !GAME.ifLost;
-    BOMB.y += BOMB.speedy;
-    if (scoreUpdate) {
-        BOMB.y = 0;
-        BOMB.x = Math.floor(Math.random() * (GAME.width - BOMB.size));
-        PLAYER.score++;
-        console.log("score: " + PLAYER.score);
-    }
-    if (losePositionX && losePositionY && !GAME.ifLost) {
-        GAME.ifLost = true;
-        alert("lose!");
-    }
+function drawInfoWindow() {
+    canvasContext.fillStyle = "black";
+    canvasContext.beginPath();
+    canvasContext.rect(InfoWindow.x, 0, InfoWindow.width, InfoWindow.height);
+    canvasContext.fill();
+    canvasContext.fillStyle = "white";
+    canvasContext.font = "30px serif";
+    canvasContext.fillText("Your score:", InfoWindow.x + 10, 50);
+    canvasContext.fillText(PLAYER.score, InfoWindow.x + 10, 85);
+    canvasContext.fillText("Your lives:", InfoWindow.x + 10, 120);
+    canvasContext.fillText(PLAYER.lives, InfoWindow.x + 10, 155);
 }
+
+var limit = 20;
+
+function updateBombs() {
+    var i = 0;
+    do {
+        BOMBS[i].y += BOMBS[i].speedy;
+        var losePositionY = BOMBS[i].y + BOMBS[i].size / 2 >= PLAYER.y;
+        var losePositionX = (BOMBS[i].x - BOMBS[i].size / 2 <= PLAYER.x + PLAYER.width) && (BOMBS[i].x + BOMBS[i].size / 2 >= PLAYER.x);
+        var scoreUpdate = (BOMBS[i].y >= GAME.height - BOMBS[i].size) && !GAME.ifLost;
+        var updateBomb = (BOMBS[i].y >= GAME.height)
+
+        if (scoreUpdate) {
+            BOMBS[i].y = 20;
+            BOMBS[i].x = Math.floor(Math.random() * (GAME.width - BOMBS[i].size));
+            PLAYER.score++;
+            BOMBS[i].speedy = Math.floor(Math.random() * 20 + 5);
+            if (PLAYER.score > limit) {
+                var initX = Math.floor(Math.random() * (GAME.width - bombSize));
+                var initSpeed = Math.floor(Math.random() * 20 + 5);
+                BOMBS[countOfBombs] = {
+                    x: initX,
+                    y: 0,
+                    speedy: initSpeed,
+                    size: 20,
+                }
+                countOfBombs++;
+                limit += limit;
+            }
+            console.log("score: " + PLAYER.score);
+        }
+        if (losePositionX && losePositionY && !GAME.ifLost) {
+            if (PLAYER.lives === 0) {
+                GAME.ifLost = true;
+            } else {
+                PLAYER.lives -= 1;
+                BOMBS[i].y = 0;
+                BOMBS[i].x = Math.floor(Math.random() * (GAME.width - BOMBS[i].size));
+            }
+        }
+        i++;
+    }
+    while (i < countOfBombs)
+}
+
 
 function drawFrame() {
     canvasContext.clearRect(0, 0, GAME.width, GAME.height);
     drawBackground();
     drawPlayer();
     drawBomb();
+    drawInfoWindow()
 }
 
 function updatePlayer() {
@@ -76,23 +144,32 @@ function updatePlayer() {
 
 function initEventListeners() {
     window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("keydown", onKeyDown)
 }
 
 function onMouseMove(event) {
     if (event.clientX + PLAYER.width < GAME.width) {
         PLAYER.x = event.clientX;
-    }
-    else {
+    } else {
         PLAYER.x = GAME.width - PLAYER.width;
+    }
+}
+function onKeyDown(event) {
+    if ((event.key === "ArrowLeft") && (PLAYER.x > 0)) {
+        PLAYER.x -= PLAYER.speedX;
+    }
+    if (event.key === "ArrowRight" && (PLAYER.x + PLAYER.width < GAME.width)) {
+        PLAYER.x += PLAYER.speedX;
     }
 }
 
 function play() {
     drawFrame();
-    updateBomb();
+    updateBombs();
     updatePlayer();
     requestAnimationFrame(play);
 }
 
+InitBombs();
 initEventListeners();
 play();
